305a65b8e71e454079899ce6a086745bc5f05219
Deepraj Podder <deeprajpodder7@gmail.com>
2022-12-07 23:58:22 +0530

    Delete Banking_Management_System/bms directory

--- Banking_Management_System/bms/WrongAccnoException.java
+++
@@ -1,7 +0,0 @@
-package com.banking.bms;
-
-public class WrongAccnoException extends Exception {
-public WrongAccnoException(String str) {
-	super(str);
-}
-}
--- Banking_Management_System/bms/WrongpinException.java
+++
@@ -1,7 +0,0 @@
-package com.banking.bms;
-
-public class WrongpinException extends Exception {
-public WrongpinException(String str) {
-	super(str);
-}
-}
